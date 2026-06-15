69c33afb43e019cfa492b959649c37d72defea5e
Moningi Nidhi <nidhimoningi2556@gmail.com>
2026-06-15 23:07:46 +0530

    Feat: initialize React project with Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-300 text-4xl font-bold text-center text-rose-600  w-3/4 mt-10 mx-auto p-5 rounded-lg shadow-xl ">
+      ReviewSense AI
+    </div>
+  );
+}     
+
+export default App;
