39079b86307063bf549374df1cc67354256d9f7f
Moningi Nidhi <nidhimoningi2556@gmail.com>
2026-06-20 23:47:18 +0530

    fix: fixed the routing in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,24 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className=" flex flex-col md:flex-row items-center justify-between px-4 md:px-8 py-4 shadow-md gap-4">
       <h1 className="text-2xl font-bold text-green-600">
         ReviewSense AI
       </h1>
-
+      
       <ul className="flex flex-wrap justify-center gap-6 font-medium">
-        <li ><a href="/">Home</a></li>
-        <li ><a href="/pages/About">About</a></li>
-        <li ><a href="/pages/Dashboard">Dashboard</a></li>
+        <li ><Link to="/">Home</Link></li>
+        <li ><Link to="/pages/About">About</Link></li>
+        <li ><Link to="/pages/Dashboard">Dashboard</Link></li>
       </ul>
 
-      <button className="bg-green-500 hover:bg-green-600 text-white text-white px-4 py-2 rounded-lg">
-        <a href="/pages/Login" className="text-white">
+      
+      <Link to="/pages/Login" className="text-white">
+        <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg">
           Login
-        </a>
-      </button>
+        </button>
+      </Link>
     </nav>
   );
 }
