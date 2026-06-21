a3acc30656173f6627cf8620f03797ae64d11ee7
Moningi Nidhi <nidhimoningi2556@gmail.com>
2026-06-22 00:34:47 +0530

    feat: Export UI components through index.js

---
+++ frontend/src/components/ui/index.jsx
@@ -0,0 +1,13 @@
+export { default as Button } from "./Button";
+export { default as Input } from "./Input";
+export { default as Modal } from "./Modal";
+export { default as Loader } from "./Loader";
+export { default as Toast } from "./Toast";
+export { default as ThemeToggle } from "./ThemeToggle";
+
+export {
+  showSuccessToast,
+  showErrorToast,
+  showLoadingToast,
+  dismissToast,
+} from "./Toast";
