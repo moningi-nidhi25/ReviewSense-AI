732ce646a25d1fb52488260d2290adf5c2cfcead
Moningi Nidhi <nidhimoningi2556@gmail.com>
2026-06-27 19:20:51 +0530

    chore(api): configure CORS for frontend integration

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
@@ -6,6 +7,19 @@ from fastapi import FastAPI
 from fastapi import HTTPException
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # In-memory data
 reviews = [
